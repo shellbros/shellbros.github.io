a82f66f2b6c5ca7770fd1db6e720cedb9aaf4268
Brabbins <david@bluewizard.com>
2026-07-02 09:12:04 -0600

    BUILD shell 2026-07-02

--- js/mapEdit.js
+++ js/mapEdit.js
@@ -106818,7 +106818,7 @@ ${this._fragmentCompilationState.compilationString}`;
   };
 
   // src/version.mjs
-  var ClientVersion = "d28d1ae3a";
+  var ClientVersion = "318b195a3";
 
   // ../node_modules/@babylonjs/core/Loading/sceneLoader.js
   var SceneLoaderAnimationGroupLoadingMode;
